75a1305c3adabcaf5bd6ca4421fdf31f25154e76
vvuylsteker <vuylsteker@protonmail.ch>
2021-06-17 11:40:22 +0200

    t

--- monerophp-master/example.php
+++ monerophp-master/example.php
@@ -7,7 +7,7 @@ use MoneroIntegrations\MoneroPhp\walletRPC;
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+//d
 require_once('src/jsonRPCClient.php');
 require_once('src/daemonRPC.php');
 
